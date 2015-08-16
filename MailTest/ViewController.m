b827855bb2192a0756985703e666f43db4dea604
skor33393 <skor33393@mail.ru>
2015-08-16 18:41:35 +0300

    Initial commit

---
+++ MailTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MailTest
+//
+//  Created by a.korenev on 16.08.15.
+//  Copyright (c) 2015 alexkorenev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
